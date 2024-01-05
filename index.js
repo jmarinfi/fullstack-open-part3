912da70b4e0b64b3b64371196f4a2580605cdde7
Joan <jmarinfi@uoc.edu>
2024-01-05 11:24:33 -0500

    Exercise 3.8

--- index.js
+++ index.js
@@ -2,7 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+var morgan = require('morgan')
+
 app.use(express.json())
+
+//app.use(morgan('tiny'))
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        '-',
+        tokens['response-time'](req, res), 'ms',
+        req.method === 'POST' ? JSON.stringify(req.body): '',
+    ].join(' ')
+}))
 
 
 let entries = [
